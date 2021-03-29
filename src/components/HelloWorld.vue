b0aa3057f619b2a40869d59f6114e1f340f0fa32
powger <306132273@qq.com>
2021-03-29 15:51:15 +0800

    2020.03.29测试001

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h1>{{ msg }}</h1>
+    <h1>{{20152405}}</h1>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
